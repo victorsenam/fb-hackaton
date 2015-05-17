4f05e982c6b15ff97204577a6abfed7108964967
Victor Sena Molero <victorsenam@gmail.com>
2015-05-17 00:16:15 -0300

    Mudando tipo de submit pro obvio padrao ne caceta

--- assets/js/search-sender.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('search-form').addEventListener('submit', function () {
-    var query = document.getElementById('inputStablishment').value;
-    
-    window.location.href = 'list.html?query=' + encodeURIComponent(query);
-    return false;
-});

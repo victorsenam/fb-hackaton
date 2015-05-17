4a51c05d699b82dc563a3a6bc0fc1a028d649497
Victor Sena Molero <victorsenam@gmail.com>
2015-05-17 03:35:27 -0300

    lalala

--- assets/js/search-getter.js
+++ assets/js/search-getter.js
@@ -1,10 +1,9 @@
 var searchGetter = function() {
     var query = window.location.href.split('query=')[1].split("&")[0];
     requestFB = "search?fields=id,name,picture{url},location{street,city}&q=" + query +"&type=place";
-    authres = FB.getAuthResponse();
     FB.api(
         requestFB,
-        {access_token: authres.accessToken},
+        {access_token: FB.getAccessToken()},
         function (response){
             teste = response;
             if (response && !response.error) {

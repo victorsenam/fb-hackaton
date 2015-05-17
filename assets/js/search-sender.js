cf93178b1beebb3961a6e7afcd78f284db079f1b
Victor Sena Molero <victorsenam@gmail.com>
2015-05-17 00:08:15 -0300

    ahahahaha

--- assets/js/search-sender.js
+++ assets/js/search-sender.js
@@ -1,4 +1,4 @@
-document.getElementById('submitter').addEventListener('click', function () {
+document.getElementById('search-form').addEventListener('submit', function () {
     var query = document.getElementById('inputStablishment').value;
     
     window.location.href = 'list.html?query=' + encodeURIComponent(query);

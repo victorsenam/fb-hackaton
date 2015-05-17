f3e7bd8de4b0f6b881f5ebe576a2969771d5e390
Victor Sena Molero <victorsenam@gmail.com>
2015-05-16 23:23:45 -0300

    Facilitando burrice do face

--- assets/js/facebook-login.js
+++ assets/js/facebook-login.js
@@ -5,22 +5,14 @@ window.fbAsyncInit = function() {
         xfbml      : true,
         version    : 'v2.3'
     });
+
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            console.log('Logged in.');
+            window.location.href = 'search.html';
+        }
+        else {
+            FB.login();
+        }
+    });
 };
-
-(function(d, s, id){
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-FB.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-        console.log('Logged in.');
-        window.location.href = 'search.html';
-    }
-    else {
-        FB.login();
-    }
-});

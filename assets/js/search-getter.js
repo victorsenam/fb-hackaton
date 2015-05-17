5b22e105ea0731b868017f391d7f9242af546f4a
Gabriel Russo <gabrielrcarmo@gmail.com>
2015-05-17 04:43:21 -0300

    aa

--- assets/js/search-getter.js
+++ assets/js/search-getter.js
@@ -1,6 +1,6 @@
 var searchGetter = function() {
     query = window.location.href.split('query=')[1].split("&")[0];
-    var requestFB = "search?fields=id,name,picture{url}&q=" + decodeURIComponent(query) +"&type=place";
+    var requestFB = "search?fields=id,name,picture{url}&q=" + query.replace("+"," ") +"&type=place";
     FB.api(
         requestFB,
         {access_token: FB.getAccessToken()},

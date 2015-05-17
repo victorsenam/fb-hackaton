41968491eb1badf136eb78546e3606bd2b25c9ce
Victor Sena Molero <victorsenam@gmail.com>
2015-05-17 03:55:14 -0300

    Templatando

--- assets/js/search-getter.js
+++ assets/js/search-getter.js
@@ -1,17 +1,20 @@
 var searchGetter = function() {
     var query = window.location.href.split('query=')[1].split("&")[0];
-    requestFB = "search?fields=id,name,picture{url}&q=" + query +"&type=place";
-    console.log(FB.getAccessToken());
+    var requestFB = "search?fields=id,name,picture{url}&q=" + query +"&type=place";
     FB.api(
         requestFB,
         {access_token: FB.getAccessToken()},
-        function (response){
-            teste = response;
+        function (response) {
             if (response && !response.error) {
                 var ourdata = response.data;
-                output = document.getElementById("tester");
+                var wrap = document.getElementById("outwrap");
+                var temp = document.getElementById("template");
+
+                var copy = temp.cloneNode();
                 for (var i = 0; i < ourdata.length; i++) {
-                    output.innerHTML += ourdata[i].name + "<br>";
+                    copy.getElementById("image").src = ourdata.picture.data.url;                    
+                    copy.getElementById("name").innerHTML = ourdata.name;
+                    wrap.appendChild(copy);
                 }
             }
         }
